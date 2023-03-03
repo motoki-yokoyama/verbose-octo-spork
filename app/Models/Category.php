b5d54e7dbcaf808c72711068f715b20f53ea9208
motoki-yokoyama <21ee438@haruka.otemon.ac.jp>
2023-03-03 20:39:12 +0900

    リレーションの実装

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    //postに対するリレーション
+    //1対多の関係なので、postsと、複数形にする
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+    
+    public function getByCategory(int $limit_count = 5)
+    {
+        return $this->posts()->with('category')->orderBy('updated_at','DESC')->paginate($limit_count);
+    }
 }
+
+
