b8c66638d8f310a58e88ecbedca79113f7e06541
motoki-yokoyama <21ee438@haruka.otemon.ac.jp>
2023-03-08 18:48:14 +0900

    ログアウト機能、ヘッダーの実装

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,28 +1,23 @@
-<!DOCTYPE HTML>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Posts</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-        <h1 class="title">
-            {{$post->title}}
-        </h1>
-        <div class="content">
-            <div class="content_post">
-                <h3>本文</h3>
-                <p class="body">{{$post->body}}</p>
-            </div>
+<x-app-layout>
+     <x-slot name="header">
+            タイピングアプリ
+    </x-slot>
+    
+    <h1 class="title">
+        {{$post->title}}
+    </h1>
+    <div class="content">
+        <div class="content_post">
+            <h3>本文</h3>
+            <p class="body">{{$post->body}}</p>
         </div>
-        <div class="edit">
-            <a href="/posts/{{ $post->id }}/edit">編集</a>
-        </div>
-        <a href="/categories/{{$post->category->id}}">{{ $post->category->name }}</a>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
-    </body>
-</html>
+    </div>
+    <div class="edit">
+        <a href="/posts/{{ $post->id }}/edit">編集</a>
+    </div>
+    <a href="/categories/{{$post->category->id}}">{{ $post->category->name }}</a>
+    <div class="footer">
+        <a href="/">戻る</a>
+    </div>
+    
+</x-app-layout>
